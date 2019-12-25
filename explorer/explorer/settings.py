d00e0cda9c66b4c48267d17d12d6d789a097dfa5
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-12-25 14:00:21 -0500

    Recreated pipfile and lock

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -11,15 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dotenv
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
