8da7da7c1df88298abef38f5291e4d193be3cdee
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-12-25 18:00:00 -0500

    Added databases variable to settings file

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'explorer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {}
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
