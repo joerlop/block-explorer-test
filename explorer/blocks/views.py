6098d08f27560fc1b888ddf92b365635ac5708dd
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-12-25 17:30:32 -0500

    Added first view to display all blocks

--- explorer/blocks/views.py
+++ explorer/blocks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from blocks.models import BlockRow
 
 # Create your views here.
+
+def blocks(request):
+    
+    blocks = BlockRow.objects.all()
+    return render(request, "db.html", {"Blocks": blocks})
