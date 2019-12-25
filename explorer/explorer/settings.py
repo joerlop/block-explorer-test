60b9a603bf8a9c4d1de085596439c3dc9fed9fc7
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2019-12-25 18:10:23 -0500

    Updated settings

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -78,19 +78,18 @@ WSGI_APPLICATION = 'explorer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {}
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
-if len(DATABASES['default']) == 0:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'new',
             'USER': 'jonathanerlich',
-            'PASSWORD': 'jerlich0811'
+            'PASSWORD': EMAIL_HOST_PASSWORD
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
